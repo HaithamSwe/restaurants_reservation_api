5a89466b6cd491449c200eb4b6e5d4d420426c24
haitham alsaeed <48700453+HaithamSwe@users.noreply.github.com>
2021-08-10 09:48:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,20 +15,6 @@ const crypto = require('crypto');
 const { Console } = require('console')
 
 
-//pool.query('CREATE TABLE employees (employee_id serial PRIMARY KEY, employee_number INT UNIQUE NOT NULL,employee_name VARCHAR ( 50 ) NOT NULL, password VARCHAR ( 128 ) NOT NULL)')
-//pool.query('CREATE TABLE reservation_timeslots (reservation_timeslot_id serial PRIMARY KEY, start_at TIME NOT NULL, end_at TIME NOT NULL)')
-//pool.query('CREATE TABLE tables (table_id serial PRIMARY KEY, number_of_chairs INT NOT NULL, is_active bool NOT NULL)')
-//pool.query('CREATE TABLE reservations (reservation_id serial PRIMARY KEY, reservation_timeslot_id INT NOT NULL, table_id INT NOT NULL, reservation_date DATE NOT NULL)')
-
-
-// pool.query("INSERT INTO employees(employee_number, employee_name, password) VALUES (" + "000" + ', '+ "'h'" + ', ' + "'" + crypto.createHash('sha512').update('222222').digest('hex')+"'" +")" , (err, res) => {
-//     if (!err)
-//         console.log(res.rows)
-//     else
-//         console.log(err)
-// })
-
-
 const port = process.env.SERVER_PORT
 app.listen(port, () => console.log('server started'))
 
